c6746c35a7942f6366095e291990b42ffa2eb5f7
Valentyn Sudak <vsudak@f1r5s14.codam.nl>
2026-06-17 15:27:04 +0200

    need to fix chmod

--- src/pages/dashboard.py
+++ src/pages/dashboard.py
@@ -13,11 +13,12 @@ def dashboard_page(user: User):
     with ui.column().style('width: 100%'):
         with ui.element('div').classes('p-2').style('width: 100%'):
             ui.label("Dashboard").classes("text-3xl font-bold mb-6")
-            ui.chat_message(('Welcom in Football Predictor app!\n',
-                            'On this page you can check your prediction results'),
+            ui.chat_message(('Welcome in Football Predictor app!\n',
+                            'On this page you can check your prediction results',
+                            'Current app status: Testing period in ON till 21st June',
+                            'On 21st the leaderboard will be nullified'),
                     name='sq.clubs.codam',
                     stamp='now',
                     avatar='https://robohash.org/ui')
         login_info(user)
         build_match_calendar()
-
--- src/services/login_info.py
+++ src/services/login_info.py
@@ -24,44 +24,45 @@ def info_prd(prd: list[Prediction]):
 
 def login_info(user: User):
     db = SessionLocal()
-    prd = db.query(Prediction)\
-        .filter(Prediction.user_id == user.id)\
-        .order_by(Prediction.id.desc())\
-        .limit(5)\
-        .all()
+    try:
+        prd = db.query(Prediction)\
+            .filter(Prediction.user_id == user.id)\
+            .order_by(Prediction.id.desc())\
+            .limit(5)\
+            .all()
 
-    avatar = user.avatar_url or '/assets/image.png'
-    with ui.column().style("width: 100%; align-items: center;"):
-        with ui.card().style(
-            "border-radius: 12px; padding: 20px; width: 100%; height: 50%;"
-            "box-shadow: 0 2px 10px rgba(0,0,0,0.08);"
-        ):
-            with ui.row().style("width: 100%; gap: 30px;"):
+        avatar = user.avatar_url or '/assets/image.png'
+        with ui.column().style("width: 100%; align-items: center;"):
+            with ui.card().style(
+                "border-radius: 12px; padding: 20px; width: 100%; height: 50%;"
+                "box-shadow: 0 2px 10px rgba(0,0,0,0.08);"
+            ):
+                with ui.row().style("width: 100%; gap: 30px;"):
 
-                # LEFT SIDE
-                with ui.column().style("flex: 1; align-items: center;"):
-                    ui.image(avatar).style(
-                        "width: 200px; height: 200px; border-radius: 50%; object-fit: cover;"
-                    )
+                    # LEFT SIDE
+                    with ui.column().style("flex: 1; align-items: center;"):
+                        ui.image(avatar).style(
+                            "width: 200px; height: 200px; border-radius: 50%; object-fit: cover;"
+                        )
 
-                    ui.label(user.login_42).classes("text-xl font-bold mt-2")
+                        ui.label(user.login_42).classes("text-xl font-bold mt-2")
 
-                    ui.label(f"Prediction Score: {user.p_score}") \
-                        .style("font-size: 1.1em; color: #666;")
+                        ui.label(f"Prediction Score: {user.p_score}") \
+                            .style("font-size: 1.1em; color: #666;")
 
-                # RIGHT SIDE
-                with ui.column().style("flex: 2;"):
-                    ui.label("Recent predictions") \
-                        .classes("text-xl font-bold mb-2")
+                    # RIGHT SIDE
+                    with ui.column().style("flex: 2;"):
+                        ui.label("Recent predictions") \
+                            .classes("text-xl font-bold mb-2")
 
-                    with ui.column().style(
-                        "border: 1px solid #eee; border-radius: 10px; "
-                        "padding: 10px; max-height: 250px; overflow-y: auto;"
-                    ):
-                        if not prd:
-                            ui.label("No predictions yet")
-                            ui.button("Go to Make Prediction page and predict football matches", on_click=lambda x: ui.navigate.to('/predict'))
-                        else:
-                            info_prd(prd)
-
+                        with ui.column().style(
+                            "border: 1px solid #eee; border-radius: 10px; "
+                            "padding: 10px; max-height: 250px; overflow-y: auto;"
+                        ):
+                            if not prd:
+                                ui.label("No predictions yet")
+                                ui.button("Go to Make Prediction page and predict football matches", on_click=lambda x: ui.navigate.to('/predict'))
+                            else:
+                                info_prd(prd)
+    finally:
         db.close()
